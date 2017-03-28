5f73dc97361c893de0249fcd44741dade1538e15
zinka <srinivas.zinka@gmail.com>
2017-03-28 11:40:15 +0530

    a few changes

--- traditional.py
+++ traditional.py
@@ -14,7 +14,7 @@ def Butterworth_g(LAs, Ws):
     g = 2 * np.sin(i1)
     g[0] = 1
     g[-1] = 1
-    return g
+    return n, g
 
 def Chebyshev_g(LAr, LAs, Ws):
     n = int(np.ceil(np.arccosh(
@@ -30,14 +30,73 @@ def Chebyshev_g(LAr, LAs, Ws):
             np.sin((2 * i - 3) * np.pi / 2 / n)
         den = gamma**2 + np.sin((i - 1) * np.pi / n)**2
         g[i] = num / den / g[i - 1]
-    return g
+    return n, g
+
+def g2LC(g, Z0=50, fc=1e9, ladder_type='LC'): # only for LPFs
+    if(ladder_type == 'LC'):
+        g[1:-1:2] = g[1:-1:2] * Z0 / (2 * np.pi * fc)
+        g[2:-1:2] = g[2:-1:2] / Z0 / (2 * np.pi * fc)
+        g[0] = Z0
+        g[-1] = g[-1] * Z0
+        g2LC = g
+    if(ladder_type == 'CL'):
+        g[1:-1:2] = g[1:-1:2] / Z0 / (2 * np.pi * fc)
+        g[2:-1:2] = g[2:-1:2] * Z0 / (2 * np.pi * fc)
+        g[0] = Z0
+        g[-1] = g[-1] * Z0
+        g2LC = g
+    return g2LC
+
+def stepped_impedance(g2LC, Z0_high, Z0_low, lg_high, lg_low, fc, ladder_type='LC', optimize=False):
+    LC_ang = np.zeros_like(g2LC)
+    if(ladder_type == 'LC'):
+        g2LC[1:-1:2] = (lg_high / 2 / np.pi) * np.arcsin(2 *
+                                                         np.pi * fc * g2LC[1:-1:2] / Z0_high)
+        LC_ang[1:-1:2] = g2LC[1:-1:2] * (2 * np.pi / lg_high)
+        g2LC[2:-1:2] = (lg_low / 2 / np.pi) * np.arcsin(2 *
+                                                        np.pi * fc * g2LC[2:-1:2] * Z0_low)
+        LC_ang[2:-1:2] = g2LC[2:-1:2] * (2 * np.pi / lg_low)
+        LC_len = g2LC
+    if(ladder_type == 'CL'):
+        pass
+    if(optimize):
+        pass # implement (5.4) from J. S. Hong
+
+    return LC_len, LC_ang
+
+def stub_LPF():
+    # Implement Fig. 5.3 from J. S. Hong
+    pass
+
+def g2K(g, LC_values=np.ones_like(g), Z0=1, ZL=1, ladder_type='L'):
+    pass
 
 if __name__ == '__main__':
 
-    print(Butterworth_g(LAs=40, Ws=2))
-    print(Chebyshev_g(LAr=0.1, LAs=40, Ws=2))
+    # print(Butterworth_g(LAs=40, Ws=2))
+    # Chebyshev_g(LAr=0.1, LAs=40, Ws=2)
 
-# ==============================================================================
-# Notes Section
-# ==============================================================================
-# TODO implement elliptic from QUCS  
+    # ==========================================================================
+    # J. S. Hong, Ch.5, LPF Excercise
+    # ==========================================================================
+
+    # g = Chebyshev_g(LAr=0.1, LAs=40, Ws=6)[1]
+    # g2LC = g2LC(g, Z0=50, fc=1e9, ladder_type='LC')
+    # LC_len, LC_ang = stepped_impedance(
+    #     g2LC, Z0_high=93, Z0_low=24, lg_high=118e-3, lg_low=105e-3, fc=1e9)
+    # print(LC_ang * 180 / np.pi)
+    # print(LC_len)
+
+    # ==========================================================================
+    # J. S. Hong, Ch.4, g to K conversion, Fig. 3.18
+    # ==========================================================================
+
+    g = Chebyshev_g(LAr=0.1, LAs=40, Ws=6)[1]
+
+
+
+    # ==========================================================================
+    # Notes Section
+    # ==========================================================================
+    # TODO implement elliptic from QUCS
+    # TODO
