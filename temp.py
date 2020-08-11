5dd566d445d34b1533fca8713e01e0ed47b20b58
zinka <srinivas.zinka@gmail.com>
2020-08-11 15:55:19 +0530

    reorganization started ... a few traditional filters have been added

--- temp.py
+++ temp.py
@@ -1,13 +1,135 @@
+from sympy import nsolve, exp, Symbol
+from sympy.core import symbol
+import sympy as sym
 
 
-def g2LC(g, Z0=50, fc=1e9, ladder_type='LC'): # only for LPFs
-    if(ladder_type == 'LC'):
+def transformation_LP():
+    return
+
+
+def transformation_HP():
+    return
+
+
+def transformation_BP():
+    return
+
+
+def transformation_BS():
+    return
+
+
+def g2MQ_BPF(g, FBW):
+    pass
+
+
+def stepped_impedance(LC, Z0_high, Z0_low, lg_high, lg_low, fc, ladder_type="LC"):
+    """
+    Function to convert LC values into TX line physical lengths ... see Sec. 5.1.1, page 114
+    @param LC: LC or CL values as an array
+    @param Z0_high: Z0 of inductor line
+    @param Z0_low: Z0 of capacitor line
+    @param lg_high: guided wavelength of inductor line @ cut-off frequency
+    @param lg_low: guided wavelength of capacitor line @ cut-off frequency
+    @param fc: cut-off frequency
+    @param ladder_type: "LC" or "CL"
+    """
+    LC_len_guess = zeros_like(LC)
+    if ladder_type == "LC":
+        LC_len_guess[1:-1:2] = (lg_high / 2 / pi) * arcsin(
+            2 * pi * fc * LC[1:-1:2] / Z0_high
+        )
+        LC_len_guess[2:-1:2] = (lg_low / 2 / pi) * arcsin(
+            2 * pi * fc * LC[2:-1:2] * Z0_low
+        )
+    if ladder_type == "CL":
+        LC_len_guess[1:-1:2] = (lg_low / 2 / pi) * arcsin(
+            2 * pi * fc * LC[1:-1:2] * Z0_low
+        )
+        print(LC_len_guess[1:-1:2])
+        LC_len_guess[2:-1:2] = (lg_high / 2 / pi) * arcsin(
+            2 * pi * fc * LC[2:-1:2] / Z0_high
+        )
+
+    #     lL = Symbol("lL")
+    #     lC = Symbol("lC")
+    #     f1 = (
+    #         Z0_high * sym.sin(2 * pi * lL / lg_high)
+    #         + Z0_low * sym.tan(pi * lC / lg_low)
+    #         - 2 * pi * fc * LC[1]
+    #     )
+    #     f2 = (
+    #         sym.sin(2 * pi * lC / lg_low) / Z0_low
+    #         + 2 * sym.tan(pi * lL / lg_high) / Z0_high
+    #         - 2 * pi * fc * LC[2]
+    #     )
+    #     print([LC_len_guess[1], LC_len_guess[2]])
+    #     print(nsolve([f1, f2], [lL, lC], [LC_len_guess[1], LC_len_guess[2]]))
+
+    return LC_len_guess
+
+    # ==========================================================================
+    # J. S. Hong, Ch.5, LPF Excercise
+    # ==========================================================================
+
+    #     g = Chebyshev_g(LAr=0.1, LAs=40, Ws=6)[1]
+    #     LC = g2LC_LPF(g, Z0=50, fc=1e9, ladder_type="LC")
+    #     LC_len = stepped_impedance(
+    #         LC, Z0_high=93, Z0_low=24, lg_high=118e-3, lg_low=105e-3, fc=1e9
+    #     )
+    #     print(LC_len)
+
+    g = Butterworth_g(LAs=20, Ws=1.5)
+    print("order: ", g[0])
+    print("parameters: ", g[1])
+    g2S(g=g[1], Wstart=0.0001, Wstop=3, npoints=1001, dB_limit=-70)
+
+    # Pozar's example ... ex 8.6, page 424
+    g = Butterworth_g(LAs=20, Ws=1.5)[1]
+    LC = g2LC_LPF(g, Z0=50, fc=2.5e9, ladder_type="CL")
+    print("LC values:\n", LC)
+    LC_len = stepped_impedance(
+        LC,
+        Z0_high=120,
+        Z0_low=20,
+        lg_high=118e-3,
+        lg_low=105e-3,
+        fc=1e9,
+        ladder_type="CL",
+    )
+
+
+# def g2KJ_HP(g, LC_values, ZY0=1, ZYL=1, plot):
+#
+# def g2KJ_BP(g, LC_values, ZY0=1, ZYL=1, plot):
+#
+# def g2KJ_BS(g, LC_values, ZY0=1, ZYL=1, plot):
+
+
+# def g2LC_LPF(g, Z0=50, fc=1e9, ladder_type="LC"):  # only for LPFs
+#     if ladder_type == "LC":
+#         g[1:-1:2] = g[1:-1:2] * Z0 / (2 * pi * fc)
+#         g[2:-1:2] = g[2:-1:2] / Z0 / (2 * pi * fc)
+#         g[0] = Z0
+#         g[-1] = g[-1] * Z0
+#         g2LC = g
+#     if ladder_type == "CL":
+#         g[1:-1:2] = g[1:-1:2] / Z0 / (2 * pi * fc)
+#         g[2:-1:2] = g[2:-1:2] * Z0 / (2 * pi * fc)
+#         g[0] = Z0
+#         g[-1] = g[-1] * Z0
+#         g2LC = g
+#     return g2LC
+
+
+def g2LC(g, Z0=50, fc=1e9, ladder_type="LC"):  # only for LPFs
+    if ladder_type == "LC":
         g[1:-1:2] = g[1:-1:2] * Z0 / (2 * np.pi * fc)
         g[2:-1:2] = g[2:-1:2] / Z0 / (2 * np.pi * fc)
         g[0] = Z0
         g[-1] = g[-1] * Z0
         g2LC = g
-    if(ladder_type == 'CL'):
+    if ladder_type == "CL":
         g[1:-1:2] = g[1:-1:2] / Z0 / (2 * np.pi * fc)
         g[2:-1:2] = g[2:-1:2] * Z0 / (2 * np.pi * fc)
         g[0] = Z0
@@ -15,23 +137,32 @@ def g2LC(g, Z0=50, fc=1e9, ladder_type='LC'): # only for LPFs
         g2LC = g
     return g2LC
 
-def stepped_impedance(g2LC, Z0_high, Z0_low, lg_high, lg_low, fc, ladder_type='LC', optimize=False):
-    LC_ang = np.zeros_like(g2LC)
-    if(ladder_type == 'LC'):
-        g2LC[1:-1:2] = (lg_high / 2 / np.pi) * np.arcsin(2 *
-                                                         np.pi * fc * g2LC[1:-1:2] / Z0_high)
-        LC_ang[1:-1:2] = g2LC[1:-1:2] * (2 * np.pi / lg_high)
-        g2LC[2:-1:2] = (lg_low / 2 / np.pi) * np.arcsin(2 *
-                                                        np.pi * fc * g2LC[2:-1:2] * Z0_low)
-        LC_ang[2:-1:2] = g2LC[2:-1:2] * (2 * np.pi / lg_low)
-        LC_len = g2LC
-    if(ladder_type == 'CL'):
-        pass
-    if(optimize):
-        pass # implement (5.4) from J. S. Hong
-
-    return LC_len, LC_ang
 
 def stub_LPF():
     # Implement Fig. 5.3 from J. S. Hong
     pass
+
+
+# ==========================================================================
+# J. S. Hong, stub LPF
+# ==========================================================================
+
+# ==========================================================================
+# J. S. Hong, elliptic LPF
+# ==========================================================================
+
+# ==========================================================================
+# J. S. Hong, interdigital
+# ==========================================================================
+
+# ==========================================================================
+# J. S. Hong, combline
+# ==========================================================================
+
+# ==========================================================================
+# J. S. Hong, pseudo-combline
+# ==========================================================================
+
+# ==========================================================================
+# J. S. Hong, stub bandpass
+# ==========================================================================
--- traditional.py
+++ traditional.py
@@ -1,93 +1,401 @@
 #! /usr/bin/env python
 
 # Author: Srinivasa Rao Zinka (srinivas . zinka [at] gmail . com)
-# Copyright (c) 2017 Srinivasa Rao Zinka
-# License: New BSD License.
+# Copyright (c) 2020 Srinivasa Rao Zinka
+# License: New BSD License
 
-from __future__ import division
-import numpy as np
+from matplotlib import pyplot as plt
+from numpy import (
+    pi,
+    ceil,
+    log,
+    arange,
+    sin,
+    arccosh,
+    sqrt,
+    tanh,
+    sinh,
+    ones,
+    r_,
+    arctan,
+    arctan2,
+    array,
+)
+from warnings import warn
+from pathspec import __updated__
+from coupling import cutoff
+import skrf as rf
+
+__updated__ = "11-08-2020"
+
 
 def Butterworth_g(LAs, Ws):
-    n = np.ceil(np.log(10**(0.1 * LAs) - 1) / (2 * np.log(Ws)))
-    i = np.arange(n + 2)
-    i1 = (2 * i - 1) * np.pi / 2 / n
-    g = 2 * np.sin(i1)
+    """
+    Function to obtain low pass parameters of Butterworth filters (Sec. 3.2.1, page 40)
+    @param LAs: minimum stop-band attenuation in dB at Ws
+    @param Ws: stop-band cutoff frequency
+    """
+    n = ceil(log(10 ** (0.1 * LAs) - 1) / (2 * log(Ws)))
+    i = arange(n + 2)
+    g = 2 * sin((2 * i - 1) * pi / 2 / n)
     g[0] = 1
     g[-1] = 1
     return n, g
 
+
 def Chebyshev_g(LAr, LAs, Ws):
-    n = int(np.ceil(np.arccosh(
-        np.sqrt((10**(0.1 * LAs) - 1) / (10**(0.1 * LAr) - 1))) / np.arccosh(Ws)))
-    beta = np.log(1 / np.tanh(LAr / 17.37))
-    gamma = np.sinh(beta / 2 / n)
-    g = np.ones(n + 2)
-    g[1] = (2 / gamma) * np.sin(np.pi / 2 / n)
-    g[-1] = 1 / np.tanh(beta / 4)**2 if n % 2 == 0 else 1
+    """
+    Function to obtain low pass parameters of Chebyshev filters (Sec. 3.2.2, page 40)
+    @param LAr: passband ripple in dB
+    @param LAs: minimum stop-band attenuation in dB at Ws
+    @param Ws: stop-band cutoff frequency
+    """
+    n = int(
+        ceil(
+            arccosh(sqrt((10 ** (0.1 * LAs) - 1) / (10 ** (0.1 * LAr) - 1)))
+            / arccosh(Ws)
+        )
+    )
+    beta = log(1 / tanh(LAr / 17.37))
+    gamma = sinh(beta / 2 / n)
+    g = ones(n + 2)
+    g[1] = (2 / gamma) * sin(pi / 2 / n)
+    g[-1] = 1 / tanh(beta / 4) ** 2 if n % 2 == 0 else 1
 
     for i in range(2, n + 1):
-        num = 4 * np.sin((2 * i - 1) * np.pi / 2 / n) * \
-            np.sin((2 * i - 3) * np.pi / 2 / n)
-        den = gamma**2 + np.sin((i - 1) * np.pi / n)**2
+        num = 4 * sin((2 * i - 1) * pi / 2 / n) * sin((2 * i - 3) * pi / 2 / n)
+        den = gamma ** 2 + sin((i - 1) * pi / n) ** 2
         g[i] = num / den / g[i - 1]
     return n, g
 
-def g2KJ(g, LC_values, ZY0=1, ZYL=1):
-    num1 = np.r_[ZY0, LC_values]
-    num2 = np.r_[LC_values, ZYL]
+
+def Elliptic_g():
+    return
+
+
+def Gaussian_g():
+    return
+
+
+def g2RLC_LPF(g, Z0=50, fc=1e9, ladder_type="LC"):
+    """
+    Function to transform g values to LPF LC values
+    @param g: low-pass prototype's g values
+    @param Z0: characteristic impedance of port1 and 2
+    @param fc: cut-off frequency in Hz
+    @param ladder_type: "LC" or "CL"
+    """
+    if ladder_type == "LC":
+        g[1:-1:2] = g[1:-1:2] * Z0 / g[0] / (2 * pi * fc)
+        g[2:-1:2] = g[2:-1:2] * g[0] / Z0 / (2 * pi * fc)
+    if ladder_type == "CL":
+        g[2:-1:2] = Z0 / g[0] / (2 * pi * fc) * g[2:-1:2]
+        g[1:-1:2] = g[0] / Z0 / (2 * pi * fc) * g[1:-1:2]
+    g[0] = g[0] * Z0
+    g[-1] = g[-1] * Z0
+    return g
+
+
+def RLC2S_LPF(
+    g, Wstart, Wstop, npoints=1001, dB_limit=-100, ladder_type="LC", W=True, plot=False
+):
+    """
+    Function to plot S21 and S11 from g parameters.
+    Main purpose of this function is to ensure that the g values are indeed correct.
+    @param g: g parameters as a list [g_0, g_1, ..., g_{n+1}]
+    @param start: starting frequency (avoid 0)
+    @param stop: stop frequency
+    @param npoints: number of points for plotting purpose
+    """
+    if not ladder_type == "LC":
+        warn("ladder_type must be LC for the time being")
+        return
+
+    freq = rf.Frequency(
+        start=Wstart / (2 * pi), stop=Wstop / (2 * pi), unit="Hz", npoints=npoints
+    )
+    tl_media = rf.DefinedGammaZ0(freq, z0=1, gamma=1j * freq.w / rf.c)
+
+    # GND and port elements
+    gnd = rf.Circuit.Ground(freq, name="gnd")
+    port1 = rf.Circuit.Port(freq, name="port1", z0=g[0])
+    port2 = rf.Circuit.Port(freq, name="port2", z0=g[-1])
+
+    g = g[1:-1]
+
+    # L and C elements for LC ladder
+    ckt_elements = []
+    ind_flag = True  # L starts first
+    for i in range(len(g)):
+        ckt_elements.append(
+            tl_media.inductor(g[i], name=str(i))
+        ) if ind_flag else ckt_elements.append(tl_media.capacitor(g[i], name=str(i)))
+        ind_flag = not ind_flag  # toggle between inductors and cpacitors
+
+    # node connections
+    cnx = []
+    for i in range(int(len(g) / 2) + 1):
+        if i == 0:
+            cnx.append([(port1, 0), (ckt_elements[i], 0)])  # starting
+        elif i == int(len(g) / 2):
+            if int(len(g)) % 2 == 0:  # ending ... even order case
+                cnx.append(
+                    [
+                        (ckt_elements[2 * i - 2], 1),
+                        (ckt_elements[2 * i - 1], 0),
+                        (port2, 0),
+                    ]
+                )
+            else:  # ending ... odd order case
+                cnx.append(
+                    [
+                        (ckt_elements[2 * i - 2], 1),
+                        (ckt_elements[2 * i - 1], 0),
+                        (ckt_elements[2 * i], 0),
+                    ]
+                )
+                cnx.append(
+                    [(ckt_elements[2 * i], 1), (port2, 0),]
+                )
+
+        else:  # in between
+            cnx.append(
+                [
+                    (ckt_elements[2 * i - 2], 1),
+                    (ckt_elements[2 * i - 1], 0),
+                    (ckt_elements[2 * i], 0),
+                ]
+            )
+    # GND connections ... same for even and odd cases
+    gnd_cnx = []
+    for i in range(int(len(g) / 2) + 1):
+        if i == 0:
+            gnd_cnx.append((gnd, 0))
+        elif i == int(len(g) / 2):
+            gnd_cnx.append((ckt_elements[2 * i - 1], 1))
+        else:
+            gnd_cnx.append((ckt_elements[2 * i - 1], 1))
+    cnx.append(gnd_cnx)
+
+    # create a circuit out of all the elements and connections
+    cir = rf.Circuit(cnx)
+    ntw = cir.network
+    freq = 2 * pi * ntw.frequency.f if W else ntw.frequency.f
+    if plot:
+        title = (
+            "Transfer Functions vs Angular Frequency"
+            if W
+            else "Transfer Functions vs Frequency"
+        )
+        xlabel = "w" if W else "f"
+        ylabel = "|H(w)|" if W else "|H(f)|"
+        rf.plotting.plot_rectangular(
+            freq,
+            cutoff(rf.mathFunctions.complex2dB(ntw.s[:, 1, 0]), dB_limit),
+            x_label=xlabel,
+            y_label=ylabel,
+            title=title,
+            show_legend=True,
+            label="S21",
+        )
+        rf.plotting.plot_rectangular(
+            freq,
+            cutoff(rf.mathFunctions.complex2dB(ntw.s[:, 0, 0]), dB_limit),
+            label="S11",
+        )
+
+        if not (port1.z0[0] == port2.z0[0]):
+            warn("port2 and port2 impedances are not equal")
+
+        s_ax = plt.gca()
+        s_ax.axvspan(0, 1, facecolor="r", alpha=0.2)
+        s_ax.axhspan(0, -3, facecolor="g", alpha=0.2)
+        #     s_ax.axvspan(0, 1.6, facecolor="r", alpha=0.2)
+
+        plt.show()
+    return freq, ntw.s[:, 0, 0], ntw.s[:, 1, 0]
+
+
+def g2KJ_LPF(g, LC_values, ZY0=1, ZYL=1):
+    """
+    Lowpass prototype with K or J inverters ... Sec. 3.4.2, page 57
+    L or C values are in general obtained from lowpass/highpass/bandpass/bandstop characteristics
+    X or B values need to be converted to L or C values (xi = W_0*Li and bi = W_0*C_i) ... Fig.3.20, page 59
+    @param g: lowpass prototype's g values
+    @param LC_values: L or C values (see Fig. 3.18 a and b)
+    @param ZY0: Z0 or Y0
+    @param ZYL: ZL or YL
+    """
+    num1 = r_[ZY0, LC_values]
+    num2 = r_[LC_values, ZYL]
     den1 = g[0:-1]
-    den2 = np.r_[g[1:-1], g[-1]]
-    KJ = np.sqrt((num1 * num2) / (den1 * den2))
+    den2 = r_[g[1:-1], g[-1]]
+    KJ = sqrt((num1 * num2) / (den1 * den2))
     return KJ
 
-def g2LC_LPF(g, Z0=50, fc=1e9, ladder_type='LC'): # only for LPFs
-    if(ladder_type == 'LC'):
-        g[1:-1:2] = g[1:-1:2] * Z0 / (2 * np.pi * fc)
-        g[2:-1:2] = g[2:-1:2] / Z0 / (2 * np.pi * fc)
-        g[0] = Z0
-        g[-1] = g[-1] * Z0
-        g2LC = g
-    if(ladder_type == 'CL'):
-        g[1:-1:2] = g[1:-1:2] / Z0 / (2 * np.pi * fc)
-        g[2:-1:2] = g[2:-1:2] * Z0 / (2 * np.pi * fc)
-        g[0] = Z0
-        g[-1] = g[-1] * Z0
-        g2LC = g
-    return g2LC
 
-def g2MQ_BPF(g,FBW):
-    pass
+def endcoupled_BPF_tht_Cg(g, FBW, Y0, W0):
+    """
+    Function to calculate (initial) electrical lengths and gap coupling values
+    @param g: lowpass prototype's g values
+    @param FBW: fractional bandwidth
+    @param Y0: characteristic admittance of port1 and 2
+    @param W0: BPF's center frequency (rad/s)
+    """
+    J = g2KJ_LPF(g, LC_values=pi / 2 * FBW * ones((g.size - 2,)))  # normalised wrt y0
+    B = J / (1 - J * J)
+    Cg = B / W0 * Y0
+    tht = []
+    for i in range(0, len(B) - 1):
+        tht.append(pi - 0.5 * (arctan(2 * B[i]) + arctan(2 * B[i + 1])))
+    return tht, Cg
 
-if __name__ == '__main__':
 
-    # ==========================================================================
-    # J. S. Hong, Ch.4, g to K conversion, (5.25)
-    # ==========================================================================
+def endcoupled_BPF2(tht, lg0, Y0, W0, Cp):
+    """
+    Function to provide the final corrected electrical lengths
+    @param tht: initial electrical lengths of resonators
+    @param lg0: guided wavelength
+    @param Y0: characteristic admittance of port1 and 2
+    @param W0: BPF's center frequency (rad/s)
+    @param Cp: Shunt capacitances calculated from EM solver
+    """
+    final_len = array(tht) * lg0 / 2 / pi
+    for i in range(len(tht)):
+        tmp1 = W0 * lg0 / Y0 / 2 / pi * Cp[i]
+        tmp2 = W0 * lg0 / Y0 / 2 / pi * Cp[i + 1]
+        final_len[i] = final_len[i] - tmp1 - tmp2
+    return final_len
 
-    n, g = Chebyshev_g(LAr=0.1, LAs=40, Ws=3)
-    print(g)
-    g2KJ(g, LC_values=np.ones((g.size-2,)))
+
+def parallel_coupled_BPF(g, FBW, Y0):
+    """
+    Function to calculate Z0e and Z0o for parallel-coupled BPD.
+    In addition to calculating widtha dn gaps from Z0 values, one should compensate 
+    for microstrip open ends.
+    @param g: lowpass prototype's g values
+    @param FBW: fractional bandwidth
+    @param Y0: characteristic admittance of port1 and 2
+    """
+    J = g2KJ_LPF(g, LC_values=pi / 2 * FBW * ones((g.size - 2,)))  # normalised wrt y0
+    Z0e = (1 / Y0) * (1 + J + J * J)
+    Z0o = (1 / Y0) * (1 - J + J * J)
+    return Z0e, Z0o
+
+
+def hairpin_BPF(g, FBW):
+    """
+    Function to calculate coupling coefficients and external quality factors 
+    corresponding to hair-pin BPFs 
+    @param g: lowpass prototype's g values
+    @param FBW: fractional bandwidth
+    """
+    den1 = g[1:-1]
+    den2 = r_[g[2:-1], g[-1]]
+    M = (FBW / sqrt(den1 * den2))[0:-1]  # discarding the last value
+    Qi = g[0] * g[1] / FBW
+    Qo = g[-2] * g[-1] / FBW
+    return Qi, M, Qo
+
+
+# ==============================================================================
+
+
+if __name__ == "__main__":
+
+    print("======================== Simulation Starts ========================")
 
     # ==========================================================================
     # Butterworth and Chebyshev g values
     # ==========================================================================
 
-    # print(Butterworth_g(LAs=40, Ws=2))
-    # Chebyshev_g(LAr=0.1, LAs=40, Ws=2)
+    #         print(Butterworth_g(LAs=40, Ws=2))  # Sec. 3.2.1, page 40
+    #         print(Chebyshev_g(LAr=0.1, LAs=40, Ws=2))  # Sec. 3.2.2, page 42
+
+    # ===========================================================================
+    # Plotting H(jw) from g values
+    # ===========================================================================
+
+    #         g = Butterworth_g(LAs=50, Ws=2)
+    #         print("order: ", g[0])
+    #         print("parameters: ", g[1])
+
+    #         g = Chebyshev_g(LAr=0.1, LAs=70, Ws=2)
+    #         print("order: ", g[0])
+    #         print("parameters: ", g[1])
+
+    #         g2S(g=g[1], Wstart=0.0001, Wstop=3, npoints=1001, dB_limit=-70)
+
+    # ===========================================================================
+    # g to RLC values and plot w.r.t f
+    # ===========================================================================
+
+    #     g = Chebyshev_g(LAr=0.1, LAs=40, Ws=6)[1]
+    #     RLC2S(g, Wstart=0.0001, Wstop=3, npoints=1001, dB_limit=-70, plot=True)
+    #
+    #     RLC = g2RLC_LPF(g, Z0=50, fc=1e9, ladder_type="LC")
+    #     RLC2S(
+    #         RLC,
+    #         Wstart=0.0001,
+    #         Wstop=2 * pi * 3e9,
+    #         npoints=1001,
+    #         dB_limit=-70,
+    #         W=False,
+    #         plot=True,
+    #     )
 
     # ==========================================================================
-    # J. S. Hong, Ch.5, LPF Excercise
+    # J. S. Hong, Ch.4, g to K conversion, (based on example given in Sec. 5.2.3)
     # ==========================================================================
 
-    # g = Chebyshev_g(LAr=0.1, LAs=40, Ws=6)[1]
-    # g2LC = g2LC_LPF(g, Z0=50, fc=1e9, ladder_type='LC')
-    # LC_len, LC_ang = stepped_impedance(
-    #     g2LC, Z0_high=93, Z0_low=24, lg_high=118e-3, lg_low=105e-3, fc=1e9)
-    # print(LC_ang * 180 / np.pi)
-    # print(LC_len)
+    #     n, g = Chebyshev_g(LAr=0.1, LAs=40, Ws=3)
+    #     print(n, g)
+    #     KJ = g2KJ(g, LC_values=ones((g.size - 2,)))  # -2 because of g_0 and g_{n+1}
+    #     print(KJ)
 
     # ==========================================================================
-    # Notes Section
+    # J. S. Hong, end coupled, Sec. 5.2.1, page 125
     # ==========================================================================
-    # TODO implement elliptic from QUCS
-    # TODO
+
+    #     g = Chebyshev_g(LAr=0.1, LAs=20, Ws=3)[1]
+    #     print(g)
+    #     RLC2S_LPF(g, Wstart=0.0001, Wstop=3, npoints=1001, dB_limit=-70, plot=False)
+    #     [tht, Cg] = endcoupled_BPF_tht_Cg(g, 0.028, Y0=1 / 50, W0=2 * pi * 6e9)
+    #     print(tht)
+    #     print(Cg)
+    #     Cp = [0.0049e-12, 0.0457e-12, 0.0457e-12, 0.0049e-12]
+    #     endcoupled_BPF2(tht, lg0=18.27e-3, Y0=1 / 50, W0=2 * pi * 6e9, Cp=Cp)
+
+    # ===========================================================================
+    # J. S. Hong, parallel-coupled, Sec. 5.2.2, page 129
+    # ===========================================================================
+
+    #     g = Chebyshev_g(LAr=0.1, LAs=40, Ws=3)[1]
+    #     print(g)
+    #     RLC2S_LPF(g, Wstart=0.0001, Wstop=3, npoints=1001, dB_limit=-70, plot=False)
+    #     parallel_coupled_BPF(g, FBW=0.15, Y0=1 / 50)
+
+    # ==========================================================================
+    # J. S. Hong, hair-pin filter, Sec. 5.2.3, page 131
+    # ==========================================================================
+
+    g = Chebyshev_g(LAr=0.1, LAs=40, Ws=3)[1]
+    RLC2S_LPF(g, Wstart=0.0001, Wstop=3, npoints=1001, dB_limit=-70, plot=False)
+    hairpin_BPF(g, FBW=0.2)
+
+
+# ==========================================================================
+# Notes Section
+# ==========================================================================
+# TODO obtain filter phase
+# TODO stepped, LC ladder, semi lumped LPF
+# TODO end and edge coupled filters
+# composite filters from Pozar
+# Richard transformation and kuroda identies pozar
+
+# refactor everythin and commit
+
+# TODO implement elliptic and Gaussian low-pass prototype filters
+# TODO Ws calculations an actual L an C values
+# update filter design article to include these traditional filters as well
+# TODO what to do with Chebyshev even case where S21 is greater than 0dB?
